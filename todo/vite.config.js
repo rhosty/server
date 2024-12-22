550aeafff0277a8b2f95d03a18d85362487a607e
Bernhard Kulis <kulis@pixelpoint.at>
2024-12-22 21:55:17 +0100

    Revert "update vite config"
    This reverts commit 0a19c6c08424238b92ca620b14e4763d7ade18f7.

--- todo/vite.config.js
+++ todo/vite.config.js
@@ -3,23 +3,16 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import vercel from 'vite-plugin-vercel';
-
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-    vercel(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  server: {
-    port: process.env.PORT,
-  },
-
 })
