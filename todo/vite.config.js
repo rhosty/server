0a19c6c08424238b92ca620b14e4763d7ade18f7
Bernhard Kulis <kulis@pixelpoint.at>
2024-12-22 21:53:57 +0100

    update vite config

--- todo/vite.config.js
+++ todo/vite.config.js
@@ -3,16 +3,23 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import vercel from 'vite-plugin-vercel';
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    vercel(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    port: process.env.PORT,
+  },
+
 })
