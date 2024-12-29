ba02dd460f2652ba96b0de46871d86c4c00b3d92
Bernhard Kulis <kulis@pixelpoint.at>
2024-12-29 19:35:56 +0100

    last try

--- api/server.js
+++ api/server.js
@@ -141,6 +141,8 @@ app.delete('/api/tasks/:id', authenticateToken, async (req, res) => {
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
+module.exports = app;
