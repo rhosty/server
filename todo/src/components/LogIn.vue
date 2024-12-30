1e5b02b02740a43afabca69c7142079e86efde09
Bernhard Kulis <kulis@pixelpoint.at>
2024-12-30 11:10:10 +0100

    change variabel

--- todo/src/components/LogIn.vue
+++ todo/src/components/LogIn.vue
@@ -9,7 +9,7 @@ const password = ref('')
 const isRegistering = ref(false)
 const router = useRouter()
 
-const apiBaseUrl = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000'
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL || 'https://serverlienz.netlify.app/'
 console.log('API base URL:', apiBaseUrl)
 
 const handleLogin = async () => {
