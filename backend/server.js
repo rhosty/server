828f0ff83608e3bcc136ad9f17cf4e3e44ed324f
Bernhard Kulis <kulis@pixelpoint.at>
2024-12-30 12:41:52 +0100

    update node

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const sqlite3 = require('sqlite3');
-const { open } = require('sqlite');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
+// const express = require('express');
+// const cors = require('cors');
+// const bodyParser = require('body-parser');
+// const sqlite3 = require('sqlite3');
+// const { open } = require('sqlite');
+// const jwt = require('jsonwebtoken');
+// const bcrypt = require('bcryptjs');
 
-// import express from 'express';
-// import cors from 'cors';
-// import bodyParser from 'body-parser';
-// import sqlite3 from 'sqlite3';
-// import { open } from 'sqlite';
-// import jwt from 'jsonwebtoken';
-// import bcrypt from 'bcryptjs';
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
 
 const app = express();
 const SECRET_KEY = 'your_secret_key'; 
